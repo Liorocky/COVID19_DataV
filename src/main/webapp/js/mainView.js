4cf43f5dfae88cda3b0b27a8cc8c7248c822c867
WarMj <R.Donghui@outlook.com>
2020-04-04 19:49:35 +0800

    新增国内疫情趋势图

--- src/main/webapp/js/mainView.js
+++ src/main/webapp/js/mainView.js
@@ -1,159 +1,3 @@
-// var trendChartOfConfirmedCasesInChina = echarts.init(document.getElementById('element1'));
-// var latestChartOfConfirmedCasesByProvince = echarts.init(document.getElementById('element2'));
-//
-// var date = []; //日期
-// var confirmedNumInChina = []; //中国累计确诊人数
-// var provinceOrCityInChina = []; //中国各省市名称
-// var confirmedNumByProvince = []; //中国各省市累计确诊人数
-//
-//
-// // 中国累计确诊人数趋势area图
-// var trendChartOfConfirmedCasesInChinaOption = {
-//     tooltip: {
-//         trigger: 'axis',
-//         position: function (pt) {
-//             return [pt[0], '10%'];
-//         }
-//     },
-//     title: {
-//         left: 'center',
-//         text: '中国累计确诊人数趋势',
-//     },
-//     toolbox: {
-//         feature: {
-//             dataZoom: {
-//                 yAxisIndex: 'none'
-//             },
-//             restore: {},
-//             saveAsImage: {}
-//         }
-//     },
-//     xAxis: {
-//         type: 'category',
-//         boundaryGap: false,
-//         data: date
-//     },
-//     yAxis: {
-//         type: 'value',
-//         boundaryGap: [0, '100%']
-//     },
-//     dataZoom: [{
-//         type: 'inside',
-//         start: 0,
-//         end: 100,
-//         disabled: true
-//     }, {
-//         start: 0,
-//         end: 100,
-//         handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-//         handleSize: '80%',
-//         handleStyle: {
-//             color: '#fff',
-//             shadowBlur: 3,
-//             shadowColor: 'rgba(0, 0, 0, 0.6)',
-//             shadowOffsetX: 2,
-//             shadowOffsetY: 2
-//         }
-//     }],
-//     series: [
-//         {
-//             name: '确诊人数',
-//             type: 'line',
-//             smooth: true,
-//             symbol: 'none',
-//             sampling: 'average',
-//             itemStyle: {
-//                 color: 'rgb(255, 70, 131)'
-//             },
-//             areaStyle: {
-//                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-//                     offset: 0,
-//                     color: 'rgb(255, 158, 68)'
-//                 }, {
-//                     offset: 1,
-//                     color: 'rgb(255, 70, 131)'
-//                 }])
-//             },
-//             data: confirmedNumInChina
-//         }
-//     ]
-// };
-//
-// // 各省市最新确诊人数条形图
-// var latestChartOfConfirmedCasesByProvinceOption = {
-//     title: {
-//         left: 'center',
-//         text: '各省市最新确诊人数',
-//     },
-//     tooltip: {
-//         trigger: 'axis',
-//         position: function (pt) {
-//             return [pt[0], '10%'];
-//         }
-//     },
-//     xAxis: {
-//         type: 'category',
-//         data: provinceOrCityInChina
-//     },
-//     yAxis: {
-//         type: 'value',
-//         min: 'dataMin',
-//         max: 'dataMax'
-//
-//     },
-//     series: [{
-//         data: confirmedNumByProvince,
-//         type: 'bar'
-//     }]
-// };
-//
-//
-// // 使用刚指定的配置项和数据显示图表。
-// trendChartOfConfirmedCasesInChina.setOption(trendChartOfConfirmedCasesInChinaOption);
-// latestChartOfConfirmedCasesByProvince.setOption(latestChartOfConfirmedCasesByProvinceOption);
-//
-// // 异步加载中国累计确诊人数趋势数据
-// $.get('http://www.dzyong.top:3005/yiqing/history').done(function (data) {
-//     for (var i = 0; i < 70; i++) {
-//         date.push(data.data[i].date);
-//         confirmedNumInChina.push(data.data[i].confirmedNum);
-//     }
-//
-//     trendChartOfConfirmedCasesInChina.setOption({
-//         xAxis: {
-//             type: 'category',
-//             boundaryGap: false,
-//             data: date
-//         },
-//
-//         series: [{
-//             name: '确诊人数',
-//             data: confirmedNumInChina
-//         }]
-//     });
-// });
-//
-// // 异步加载各省市最新确诊人数数据
-// $.get('http://www.dzyong.top:3005/yiqing/province').done(function (data) {
-//     console.log(data);
-//     for (var i = 0; i < data.data.length; i++) {
-//         provinceOrCityInChina.push(data.data[i].provinceName);
-//         confirmedNumByProvince.push(data.data[i].confirmedNum);
-//     }
-//
-//     latestChartOfConfirmedCasesByProvince.setOption({
-//         xAxis: {
-//             type: 'category',
-//             data: provinceOrCityInChina
-//         },
-//
-//         series: [{
-//             data: confirmedNumByProvince,
-//             type: 'bar'
-//         }]
-//     });
-// });
-
 //国内基础数据
 $.get("api/getBasicData").done(function (data) {
     $("#updateTime").text("截至北京时间：" + data.updateTime);
@@ -166,7 +10,7 @@ $.get("api/getBasicData").done(function (data) {
 })
 
 //国内疫情地图
-var chinaMap = echarts.init(document.getElementById('chinaMap'))
+var chinaMap = echarts.init(document.getElementById('chinaMap'));
 var confirmed = new Array();
 var suspected = new Array();
 var confirmedOption;
@@ -288,5 +132,138 @@ function switchMap(type) {
     };
 }
 
+//国内疫情趋势图
+var trendCharts = echarts.init(document.getElementById('trendCharts'));
+var updateTime = new Array();
+var confirmedCountData = new Array(); //累计确诊
+var currentConfirmedCountData = new Array(); //现存确诊
+var deadCountData = new Array(); //累计死亡
+var curedCountData = new Array(); //累计治愈
 
+var confirmedCountOption; //累计确诊
+var currentConfirmedCountOption; //现存确诊
+var deadCountOption; //累计死亡
+var curedCountOption; //累计治愈
+$.get("api/getTimeLineData?startdate=2020-03-06&enddate=2020-03-30").done(function (data) {
+    for (var i = 0; i < data.length; i++) {
+        var date = new Date(data[i].updateTime);
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
+        updateTime.push(M+D);
+        confirmedCountData.push(Number(data[i].confirmedCount));
+        currentConfirmedCountData.push(data[i].currentConfirmedCount);
+        deadCountData.push(data[i].deadCount);
+        curedCountData.push(data[i].curedCount);
+    }
 
+    confirmedCountOption = {
+        title: {
+            text: "累计确诊",
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: updateTime
+        },
+        yAxis: {
+            type: 'value',
+            min: function (value) {
+                return value.min - 1000;
+            },
+            max: 'dataMax'
+        },
+        series: [{
+            data: confirmedCountData,
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+
+    currentConfirmedCountOption = {
+        title: {
+            text: "现存确诊",
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: updateTime
+        },
+        yAxis: {
+            type: 'value',
+            min: function (value) {
+                return value.min - 1000;
+            },
+            max: 'dataMax'
+        },
+        series: [{
+            data: currentConfirmedCountData,
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+
+    deadCountOption = {
+        title: {
+            text: "累计死亡",
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: updateTime
+        },
+        yAxis: {
+            type: 'value',
+            min: function (value) {
+                return value.min - 1000;
+            },
+            max: 'dataMax'
+        },
+        series: [{
+            data: deadCountData,
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+
+    curedCountOption = {
+        title: {
+            text: "累计治愈",
+            left: 'center'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: updateTime
+        },
+        yAxis: {
+            type: 'value',
+            min: function (value) {
+                return value.min - 1000;
+            },
+            max: 'dataMax'
+        },
+        series: [{
+            data: curedCountData,
+            type: 'line',
+            areaStyle: {}
+        }]
+    };
+
+    trendCharts.setOption(confirmedCountOption);
+})
+
+//切换趋势图
+function switchChart(type) {
+    if (type === "confirmedCount") {
+        trendCharts.setOption(confirmedCountOption);
+    } else if (type === "currentConfirmedCount") {
+        trendCharts.setOption(currentConfirmedCountOption);
+    } else if (type === "deadCount") {
+        trendCharts.setOption(deadCountOption);
+    } else if (type === "curedCount") {
+        trendCharts.setOption(curedCountOption);
+    };
+}
