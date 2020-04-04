6e31a61ced0cf7d9ed6b2a51610b90a114794b6c
WarMj <R.Donghui@outlook.com>
2020-04-04 15:16:47 +0800

    重构BasicData接口，新增国内外当天数据，来源丁香园

--- src/main/java/top/warmj/covid19_DataV/domain/BasicData.java
+++ src/main/java/top/warmj/covid19_DataV/domain/BasicData.java
@@ -1,53 +1,175 @@
 package top.warmj.covid19_DataV.domain;
 
-
-import java.sql.Timestamp;
-
 public class BasicData {
 
-  private String chinaConfirmedCount;
-  private String chinaCuredCount;
-  private String chinaDeadCount;
-  private String overseas;
-  private java.sql.Timestamp updateTime;
+  private String id;
+  private String currentConfirmedCount;
+  private String confirmedCount;
+  private String suspectedCount;
+  private String curedCount;
+  private String deadCount;
+  private String seriousCount;
+  private String suspectedIncr;
+  private String currentConfirmedIncr;
+  private String confirmedIncr;
+  private String curedIncr;
+  private String deadIncr;
+  private String seriousIncr;
+  private String foreignStatistics;
+  private String globalStatistics;
+  private String globalOtherTrendChartData;
+  private String updateTime;
 
-  public String getChinaConfirmedCount() {
-    return chinaConfirmedCount;
+  public String getId() {
+    return id;
   }
 
-  public void setChinaConfirmedCount(String chinaConfirmedCount) {
-    this.chinaConfirmedCount = chinaConfirmedCount;
+  public void setId(String id) {
+    this.id = id;
   }
 
-  public String getChinaCuredCount() {
-    return chinaCuredCount;
+
+  public String getCurrentConfirmedCount() {
+    return currentConfirmedCount;
   }
 
-  public void setChinaCuredCount(String chinaCuredCount) {
-    this.chinaCuredCount = chinaCuredCount;
+  public void setCurrentConfirmedCount(String currentConfirmedCount) {
+    this.currentConfirmedCount = currentConfirmedCount;
   }
 
-  public String getChinaDeadCount() {
-    return chinaDeadCount;
+
+  public String getConfirmedCount() {
+    return confirmedCount;
   }
 
-  public void setChinaDeadCount(String chinaDeadCount) {
-    this.chinaDeadCount = chinaDeadCount;
+  public void setConfirmedCount(String confirmedCount) {
+    this.confirmedCount = confirmedCount;
   }
 
-  public String getOverseas() {
-    return overseas;
+
+  public String getSuspectedCount() {
+    return suspectedCount;
   }
 
-  public void setOverseas(String overseas) {
-    this.overseas = overseas;
+  public void setSuspectedCount(String suspectedCount) {
+    this.suspectedCount = suspectedCount;
   }
 
-  public Timestamp getUpdateTime() {
+
+  public String getCuredCount() {
+    return curedCount;
+  }
+
+  public void setCuredCount(String curedCount) {
+    this.curedCount = curedCount;
+  }
+
+
+  public String getDeadCount() {
+    return deadCount;
+  }
+
+  public void setDeadCount(String deadCount) {
+    this.deadCount = deadCount;
+  }
+
+
+  public String getSeriousCount() {
+    return seriousCount;
+  }
+
+  public void setSeriousCount(String seriousCount) {
+    this.seriousCount = seriousCount;
+  }
+
+
+  public String getSuspectedIncr() {
+    return suspectedIncr;
+  }
+
+  public void setSuspectedIncr(String suspectedIncr) {
+    this.suspectedIncr = suspectedIncr;
+  }
+
+
+  public String getCurrentConfirmedIncr() {
+    return currentConfirmedIncr;
+  }
+
+  public void setCurrentConfirmedIncr(String currentConfirmedIncr) {
+    this.currentConfirmedIncr = currentConfirmedIncr;
+  }
+
+
+  public String getConfirmedIncr() {
+    return confirmedIncr;
+  }
+
+  public void setConfirmedIncr(String confirmedIncr) {
+    this.confirmedIncr = confirmedIncr;
+  }
+
+
+  public String getCuredIncr() {
+    return curedIncr;
+  }
+
+  public void setCuredIncr(String curedIncr) {
+    this.curedIncr = curedIncr;
+  }
+
+
+  public String getDeadIncr() {
+    return deadIncr;
+  }
+
+  public void setDeadIncr(String deadIncr) {
+    this.deadIncr = deadIncr;
+  }
+
+
+  public String getSeriousIncr() {
+    return seriousIncr;
+  }
+
+  public void setSeriousIncr(String seriousIncr) {
+    this.seriousIncr = seriousIncr;
+  }
+
+
+  public String getForeignStatistics() {
+    return foreignStatistics;
+  }
+
+  public void setForeignStatistics(String foreignStatistics) {
+    this.foreignStatistics = foreignStatistics;
+  }
+
+
+  public String getGlobalStatistics() {
+    return globalStatistics;
+  }
+
+  public void setGlobalStatistics(String globalStatistics) {
+    this.globalStatistics = globalStatistics;
+  }
+
+
+  public String getGlobalOtherTrendChartData() {
+    return globalOtherTrendChartData;
+  }
+
+  public void setGlobalOtherTrendChartData(String globalOtherTrendChartData) {
+    this.globalOtherTrendChartData = globalOtherTrendChartData;
+  }
+
+
+  public String getUpdateTime() {
     return updateTime;
   }
 
-  public void setUpdateTime(Timestamp updateTime) {
+  public void setUpdateTime(String updateTime) {
     this.updateTime = updateTime;
   }
+
 }
